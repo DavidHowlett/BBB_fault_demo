389785ec4e50af35fd78673d338063458da6be04
David Howlett <david.howlett3@gmail.com>
2017-05-04 16:43:15 +0100

    demo simplified

--- on_PC.py
+++ on_PC.py
@@ -1,40 +1,66 @@
 import serial.tools.list_ports
 
-# print the names of all the ports on the system
-for port in serial.tools.list_ports.comports():
-    print(port.device)
-# connect to the BBB serial port on OSX
-x = serial.Serial('/dev/cu.usbmodem1425')
-# repeat the string "HelloWorld" endlessly to stdout
-x.write(b'yes HelloWorld\n')
+# connect to the BBB serial port on Windows
+x = serial.Serial('COM9', xonxoff=True)
+
+x.write(b'yes "1234567"\n')
+
 while True:
     received = x.readline()
-    if received != b'HelloWorld\r\n':
+    if received != b'1234567\r\n':
         print(received)
 
-# This program prints the below lines, I get 1 error every ~700 milliseconds on OSX.
+# this prints:
 """
-/dev/cu.Bluetooth-Incoming-Port
-/dev/cu.usbmodem1425
+b'yes "1234567"\r\n'
+b'11234567\r\n'
+b'11234567\r\n'
+b'12234567\r\n'
+b'12334567\r\n'
+b'12345677\r\n'
 b'\n'
-b'HelloWorld\r\r\n'
-b'HelloW\r\n'
-b'HelloWorHelloWorld\r\n'
-b'HelHelloWorld\r\n'
-b'HelHelloWorld\r\n'
-b'\r\n'
-b'HeHelloWorld\r\n'
-b'HelloWHelloWorld\r\n'
-b'HelloWorlHelloWorld\r\n'
-b'HeHelloWorld\r\n'
-b'HelloWorldHelloWorld\r\n'
-b'HelloWHelloWorld\r\n'
-b'HelloWorHelloWorld\r\n'
-b'HelloWorldHelloWorld\r\n'
-b'HelloWHelloWorld\r\n'
-b'HelloHelloWorld\r\n'
-b'HelHelloWorld\r\n'
-b'HelHelloWorld\r\n'
-b'HHelloWorld\r\n'
-b'HelloWorldHelloWorld\r\n'
+b'\n'
+b'\n'
+b'\n'
+b'\n'
+b'\n'
+b'12345567\r\n'
+b'\n'
+b'12234567\r\n'
+b'\n'
+b'12334567\r\n'
+b'12344567\r\n'
+b'12344567\r\n'
+b'\n'
+b'\n'
+b'12344567\r\n'
+b'12234567\r\n'
+b'12345567\r\n'
+b'12345667\r\n'
+b'\n'
+b'\n'
+b'12234567\r\n'
+b'12334567\r\n'
+b'12234567\r\n'
+b'12345677\r\n'
+b'12334567\r\n'
+b'\n'
+b'12345677\r\n'
+b'1234567\r\r\n'
+b'\n'
+b'11234567\r\n'
+b'12234567\r\n'
+b'123234567\r\n'
+b'12234567\r\n'
+b'12334567\r\n'
+b'\n'
+b'12345677\r\n'
+b'1234567\r\r\n'
+b'12344567\r\n'
+b'\n'
+b'11234567\r\n'
+b'\n'
+b'12334567\r\n'
+b'12345667\r\n'
+b'12234567\r\n'
 """

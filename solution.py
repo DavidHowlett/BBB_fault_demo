014bfda236017b602a8e691bd68d4323c578f939
David Howlett <david.howlett3@gmail.com>
2017-05-16 09:38:33 +0100

    print data rate

--- solution.py
+++ solution.py
@@ -1,4 +1,5 @@
 import paramiko
+import time
 
 # setup logging
 paramiko.util.log_to_file('BBB_paramiko.log')
@@ -11,12 +12,15 @@ client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(hostname, port, username, password)
+stdin, stdout, stderr = client.exec_command('yes 1234567890', bufsize=2**24)
 print('streaming started')
-stdin, stdout, stderr = client.exec_command(
-    'yes 1234567890', bufsize=2**24)
+startTime = time.perf_counter()
+total_data = 0
 for i in range(1_000_000):
-    x = stdout.readline()
-    if x != '1234567890\n':
-        print(x)
-print('finished')
+    line = stdout.readline()
+    if line == '1234567890\n':
+        total_data += len(line)
+    else:
+        print(line)
+print(f'bytes per second: {int(total_data/(time.perf_counter()-startTime))}')
 client.close()
